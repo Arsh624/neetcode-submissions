3fd19d3505fc62292cb22d9c6a6195ff16c95d9d
Archit Shukla <75004866+Arsh624@users.noreply.github.com>
2026-09-17 14:33:25 -0700

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,40 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        """
+        bruteforce 
+        - check every pair  
+        - for i in range(len(nums)):
+            for j in range(1,len(nums))
+        
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+            
+        return []
+        """
+        
+        # hashm={}
+        # for i in range(len(nums)):# {3:0,4:1,5:2,6:3}
+        #     hashm[nums[i]]=i
+        
+        # for i in range(len(nums)):
+        #     j=target-nums[i]
+        #     if j in hashm and i!=hashm[j]:
+        #         return [i,hashm[j]]
+        # return []
+        
+        hashm={}
+
+        for i, n in enumerate(nums):
+            diff=target-n
+
+            if diff in hashm:
+                return [hashm[diff],i]
+            hashm[n]=i
+        
+      
+
+        
+
